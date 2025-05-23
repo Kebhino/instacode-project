610a3e47a5126cf49398b050e8c7bbaade900bf5
Kebhino <wrobeljakub@gmail.com>
2025-05-23 18:10:40 +0200

    Adding logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
+import { Logo } from "./assets/InstaCodeLogo";
+import { VStack } from "@chakra-ui/react";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>InstaCode</h1>
-      <div className="card">
+      <VStack align={"center"} justify="center" h="100vh">
+        <Logo boxSize={100} color="blue.400" />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
+      </VStack>
     </>
   );
 }
