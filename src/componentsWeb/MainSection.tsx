bd01dec59d73a4ab98d6de2a3441863ae6efaacb
Kebhino <wrobeljakub@gmail.com>
2025-05-28 12:25:05 +0200

    fix font sizing

--- src/componentsWeb/MainSection.tsx
+++ src/componentsWeb/MainSection.tsx
@@ -17,13 +17,17 @@ const AboutSection = () => {
       <Stack gap={4} textAlign="left" maxW="800px" mx="10" zIndex="1">
         <Heading
           as={"h1"}
-          fontSize={{ base: "2.5rem", md: "2.5rem" }}
+          fontSize={{ base: "25px", md: "35px", lg: "40px" }}
           my={10}
           color={"white"}
           lineHeight={1.3}
         >
           Dedykowane systemy <br /> IT dla Twojej firmy
-          <Text fontSize={"lg"} fontWeight={0} my={2}>
+          <Text
+            fontSize={{ base: "12px", md: "15px", lg: "20px" }}
+            fontWeight={0}
+            my={2}
+          >
             Automatyzuj. Optymalizuj. Zarządzaj.
           </Text>
         </Heading>
