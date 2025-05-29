df9e926ab41f9719876253fb9df59648454a7ace
Kebhino <wrobeljakub@gmail.com>
2025-05-29 16:43:40 +0200

    Adjustments

--- src/componentsWeb/BottomGrid.tsx
+++ src/componentsWeb/BottomGrid.tsx
@@ -1,7 +1,7 @@
 import { Box, SimpleGrid, Text, VStack, Image } from "@chakra-ui/react";
-import logoCuprum from "@/assets/logaSlider/cuprum-logo.webp";
-import logoAbart from "@/assets/logaSlider/abart-logo.webp";
-import logoZL from "@/assets/logaSlider/znanylekarz-logo.webp";
+import logoCuprum from "@/assets/logaSlider/cuprum-logo-blue.webp";
+import logoIG from "@/assets/logaSlider/instaglass-logo-blue.webp";
+import logoZL from "@/assets/logaSlider/znanylekarz-blue.webp";
 
 const BottomGrid = () => {
   return (
@@ -16,10 +16,10 @@ const BottomGrid = () => {
       placeItems="center"
     >
       <Box bg="#F6FCFE" p={5} borderRadius={10}>
-        <VStack gap={2} align="center">
-          <Image src={logoAbart} color="#00163E" />
+        <VStack gap={2} align="center" marginTop={1}>
+          <Image src={logoIG} height={"40px"} />
 
-          <Text fontSize={10} marginTop={3} textAlign={"left"}>
+          <Text marginLeft={2} fontSize={10} marginTop={3} textAlign={"left"}>
             Zinformatyzowaliśmy firmę ABART, obejmując naszym systemem każdy
             aspekt działalności firmy, od automatycznych wycen poczynając, po
             śledzenie, rozliczanie i raportowanie całego procesu instalacji
@@ -28,11 +28,16 @@ const BottomGrid = () => {
         </VStack>
       </Box>
 
-      <Box bg="#F6FCFE" p={3} borderRadius={10}>
-        <VStack gap={2} align="center">
-          <Image src={logoZL} color="#00163E" />
+      <Box
+        bg="#F6FCFE"
+        p={3}
+        borderRadius={10}
+        height={{ base: "auto", md: "auto", lg: "170px" }}
+      >
+        <VStack gap={2} align="center" marginTop={5}>
+          <Image src={logoZL} color="#00163E" height={"30px"} />
 
-          <Text fontSize={10}>
+          <Text marginLeft={2} fontSize={10} marginTop={4} textAlign={"left"}>
             Największy nasz sukces to ZnanyLekarz.pl, który został w całości
             zaprojektowany i wykonany przez nas gdy pojawił się pomysł na taki
             serwis. Nasz projekt i wykonanie okazało się wielkim sukcesem,
@@ -42,11 +47,16 @@ const BottomGrid = () => {
         </VStack>
       </Box>
 
-      <Box bg="#F6FCFE" p={5} borderRadius={10}>
-        <VStack gap={2} align="center">
-          <Image src={logoCuprum} color="#00163E" />
+      <Box
+        bg="#F6FCFE"
+        p={5}
+        borderRadius={10}
+        height={{ base: "auto", md: "auto", lg: "170px" }}
+      >
+        <VStack gap={2} align="center" marginTop={3}>
+          <Image src={logoCuprum} color="#00163E" height={"40px"} />
 
-          <Text fontSize={10}>
+          <Text marginLeft={2} fontSize={10} marginTop={2} textAlign={"left"}>
             Dla KGHM CUPRUM przez lata przygotowywaliśmy dedykowane rozwiązania,
             które dbały o efektywną wymianę informacji w ramach tej firmy przy
             jednoczesnym zapewnieniu odpowiedniej poufności.
