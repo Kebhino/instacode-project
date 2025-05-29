e22022e1872d49498a8ebc7a2bec80a0f2648918
Kebhino <wrobeljakub@gmail.com>
2025-05-29 11:22:41 +0200

    Deleting icons, fix adjustment

--- src/componentsWeb/NavBar.tsx
+++ src/componentsWeb/NavBar.tsx
@@ -1,69 +1,62 @@
 import { Logo } from "@/assets/InstaCodeLogo";
 import { Box, HStack, Spacer } from "@chakra-ui/react";
 import { NavLink } from "./NavLink";
-import { House, Mail, Scroll } from "lucide-react";
+import { House } from "lucide-react";
 
 const NavigationBar = () => {
   return (
     <Box
-      position="relative"
-      color="white"
-      overflow="hidden"
       backgroundColor="white"
-      minH="100px"
-      borderRadius={"15px 15px 0 0"}
+      borderRadius={"25px 25px 0 0"}
       boxShadow="0 2px 4px rgba(0,0,0,0.05)"
+      color="white"
+      minH="100px"
+      overflow="hidden"
+      position="relative"
     >
       <Box py={1} textAlign="center">
         <HStack
           align="center"
-          justify="space-between"
           fontSize={{ base: "sm", md: "md", lg: "lg" }}
+          justify="space-between"
           maxW={{ base: "420px", md: "1200px", lg: "1200px" }}
         >
           <Logo
-            minW={{ base: 50, md: 20 }}
-            width={{ base: 20, md: 100, lg: 100 }}
+            color="black"
             height={{ base: 20, md: 100, lg: 100 }}
             marginLeft={{ base: "none", md: 10, lg: 10 }}
-            color="black"
+            minW={{ base: 50, md: 20 }}
+            width={{ base: 20, md: 100, lg: 100 }}
           />
           <Spacer />
           <NavLink
-            mx={"auto"}
-            to="/"
-            textWrap={"nowrap"}
-            border={"sm"}
-            p={1}
-            marginRight={{ base: 2, md: 10, lg: 10 }}
-            borderRadius={"lg"}
-            borderColor={"#00163E"}
             color={"#00163E"}
+            marginRight={{ base: 2, md: 10, lg: 10 }}
+            mx={"auto"}
+            p={1}
+            textWrap={"nowrap"}
+            to="/"
           >
             <House />
           </NavLink>
           <NavLink
-            mx={"auto"}
+            color={"#00163E"}
             marginRight={{ base: 2, md: 10, lg: 10 }}
-            to="/about"
-            textWrap={"nowrap"}
-            border={"sm"}
+            mx={"auto"}
             p={1}
-            borderRadius={"lg"}
-            borderColor={"#00163E"}
+            textWrap={"nowrap"}
+            to="/about"
           >
-            <Scroll /> O nas
+            O nas
           </NavLink>
           <NavLink
-            mx={"auto"}
+            color={"#00163E"}
             marginRight={{ base: 2, md: 10, lg: 10 }}
-            to="/kontakt"
-            border={"sm"}
+            mx={"auto"}
             p={1}
-            borderRadius={"lg"}
-            borderColor={"#00163E"}
+            to="/kontakt"
           >
-            <Mail /> Kontakt
+            Kontakt
           </NavLink>
         </HStack>
       </Box>
