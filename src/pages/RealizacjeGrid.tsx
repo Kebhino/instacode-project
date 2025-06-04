c4c46ef7703aef5b1310ccc3fe03096951d82e07
Kebhino <wrobeljakub@gmail.com>
2025-06-04 15:20:47 +0200

    przerobienie podstrony z realizacjami

--- src/pages/RealizacjeGrid.tsx
+++ src/pages/RealizacjeGrid.tsx
@@ -1,16 +1,39 @@
-import FooterComp from "@/componentsWeb/FooterComp";
+import Footer from "@/componentsWeb/FooterComp";
 import MainSection from "@/componentsWeb/MainSection";
-import NavBar from "@/componentsWeb/NavBar";
-
+import NavigationBar from "@/componentsWeb/NavBar";
 import ProjectGridList from "@/componentsWeb/Projects/ProjectGridList";
+
+import { Grid, GridItem } from "@chakra-ui/react";
+
 
 const RealizacjeGrid = () => {
   return (
     <>
-      <NavBar />
-      <MainSection />
-      <ProjectGridList />
-      <FooterComp />
+    <Grid
+          templateRows="auto 1fr auto auto"
+          templateColumns={"1fr"}
+          minH="100vh"
+          maxW={"1200px"}
+          mx={"auto"}
+          justifyContent={"center"}
+        >
+          <GridItem>
+            <NavigationBar />
+          </GridItem>
+          <GridItem color={"black"}>
+            <MainSection />
+       <ProjectGridList/>
+          </GridItem>
+    
+          <GridItem bg={"white"}>
+      
+          </GridItem>
+          <GridItem bg="#00163E">
+            <Footer />
+          
+          </GridItem>
+        </Grid>
+     
     </>
   );
 };
