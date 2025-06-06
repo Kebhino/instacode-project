64adb0e6127270e0104f27ecac2c7d381c9f8d57
Kebhino <wrobeljakub@gmail.com>
2025-06-06 13:33:56 +0200

    Błedy z konsoli poprawione.

--- src/Layout/Hero/TextHero.tsx
+++ src/Layout/Hero/TextHero.tsx
@@ -1,26 +1,35 @@
-import { Text } from "@chakra-ui/react";
+import { Text, Box } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
-const MotionText = motion(Text);
+const MotionText = motion.create(Text);
 
 const TextHero = () => {
   return (
-    <MotionText
-      fontSize={{ base: "25px", md: "35px", lg: "40px" }}
-      fontWeight="bold"
-      color="white"
-      mt={10}
-      ml={10}
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, ease: "easeOut" }}
-      whiteSpace="pre-line"
-    >
-      Dedykowane systemy IT{"\n"} dla Twojej firmy
-      <Text fontSize="xl" mt={4} color={"white"} mb={4}>
-        Autmatyzuj. Optymalizuj. Zarządzaj.{" "}
-      </Text>
-    </MotionText>
+    <Box ml={{ base: 5, md: 10 }} mt={{ base: 5, md: 10 }}>
+      <MotionText
+        fontSize={{ base: "25px", md: "35px", lg: "40px" }}
+        fontWeight="bold"
+        color="white"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        whiteSpace="pre-line"
+      >
+        Dedykowane systemy IT{"\n"}dla Twojej firmy
+      </MotionText>
+
+      <MotionText
+        fontSize="xl"
+        mt={4}
+        color="white"
+        mb={4}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
+      >
+        Automatyzuj. Optymalizuj. Zarządzaj.
+      </MotionText>
+    </Box>
   );
 };
 
